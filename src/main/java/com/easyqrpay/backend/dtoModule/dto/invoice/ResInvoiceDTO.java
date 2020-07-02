3913b4b29529cc8c864fc55cabc735ebf0f2efb6
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-07-02 18:20:09 +0500

    - DTOs extends Consumer - DTOs extends Invoice

---
+++ src/main/java/com/easyqrpay/backend/dtoModule/dto/invoice/ResInvoiceDTO.java
@@ -0,0 +1,16 @@
+package com.easyqrpay.backend.dtoModule.dto.invoice;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class ResInvoiceDTO {
+    private Long id;
+    private String qr;
+    private Double totalAmount;
+    private Date generatedDate;
+    private Date dueDate;
+}
