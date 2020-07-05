4d6d1a275efd053e8fc2fcfce2ef1e36bbe02564
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-07-05 16:10:31 +0500

    - https://github.com/easyqrpay/customer/issues/25

--- src/main/java/com/easyqrpay/modules/dtoModule/dto/types/TransactionStatus.java
+++ src/main/java/com/easyqrpay/modules/dtoModule/dto/types/TransactionStatus.java
@@ -1,10 +1,18 @@
 package com.easyqrpay.modules.dtoModule.dto.types;
 
+//        1 ---------> INITIALIZE
+//        2 ---------> All
+//        3 ---------> Pending
+//        4 ---------> Paid
+//        5 ---------> Void
+//        6 ---------> Expired
+
+
 public enum TransactionStatus {
+    INITIALIZE,
     ALL,
-    INITIALIZATION,
     PENDING,
     PAID,
-    EXPIRED,
-    VOIDED
+    VOID,
+    EXPIRED
 }
